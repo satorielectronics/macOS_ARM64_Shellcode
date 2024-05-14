c7fa391474fd92ca410a647e0ae4589146c74dec
timmy <moequalitycontrol@gmail.com>
2024-05-14 17:49:13 -0600

    Update extract.sh
    Updated exctact.sh to properly extract sc. Original script didn't run on fresh Apple M3 Pro install.

--- helper/extract.sh
+++ helper/extract.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-if [[ $# -ne 1 ]] ; then
+# Check if the input file exists
+if [ ! -f "$1" ]; then
     echo "Usage: $0 <shellcode.macho|shellcode.o>"
     exit 1
 fi
 
-for s in $(objdump -d $1 | grep -E '[0-9a-f]+:' | cut -f 1 | cut -d : -f 2) ; do
-    echo -n $s | awk '{for (i = 7; i > 0; i -= 2) {printf "\\x" substr($0, i, 2)}}'
-done
-
-echo
+# Apply the logic to each section of the object file
+for s in $(objdump -d "$1" | grep -E '[0-9a-f]+:' | cut -f 1 | cut -d : -f 2); do
+    echo -n "$s" | awk '{for (i = 7; i > 0; i -= 2) {printf "\\x" substr($0, i, 2)}}'
+done 
